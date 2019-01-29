a9abae6a13c97cde9b35ce9092ff48c8520b849e
awais69 <awais69169@gmail.com>
2019-01-30 01:11:36 +0500

    controller angular

--- js/app.js
+++ js/app.js
@@ -2,6 +2,25 @@
 
 var events = angular.module("events",[]);
 
-events.controller("categoriesController",function($scope){
-	$scope.x = "hello mic check";
+events.controller("categoriesControl",function($scope){
+	$scope.showCat = function(event){
+		var clickedRefer = event.currentTarget;
+		var category = clickedRefer.innerHTML;
+		
+		if(category == "The PSL"){
+			$scope.eventsArray = ["PSL1","PSL2","PSL3"];
+		}else{
+			$scope.eventsArray = ["ICC1","Icc2","ICC3"];
+		}
+		
+	};
+	
+	$scope.eventsArray = [
+		{heading:'PSL',year:'2015',
+		 wiki:'www.wiki.com/psl',
+		 imgSrc:'../images/myimg.jpg'},
+		{heading:'PSL',year:'2017',
+		 wiki:'www.wiki.com/psl17',
+		 imgSrc:'../images/myimg17.jpg'}
+		];
 });

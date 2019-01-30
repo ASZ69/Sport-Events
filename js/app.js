67b32a4e401b485d2a0ca21b2d68f349811202e8
awais69 <awais69169@gmail.com>
2019-01-30 19:39:43 +0500

    some commit

--- js/app.js
+++ js/app.js
@@ -7,46 +7,116 @@ events.controller("categoriesControl",function($scope,$filter){
 		var clickedRefer = event.currentTarget;
 		var category = clickedRefer.innerHTML;
 		
-		$scope.eventsArray = $filter('filter')(dataList, {heading: category});
+		$scope.eventsArray = $filter('filter')(dataList, {name: category});
 	};
 	
-	$scope.eventsArray = $filter('filter')(dataList, {heading: 'The PSL'});
+	$scope.eventsArray = $filter('filter')(dataList, {name: 'The PSL'});
 });
 
-var dataList = [
-		{heading:'The PSL',season:'2015',
-		 wiki:'www.wiki.com/psl',
-		 imgSrc:'../images/myimg.jpg',
-		 moreDetails:{
-			 images:[
-				 '../images/my.jpg',
-			 	 'asdasda',
-			 	 'adsfda'],
-			 scoreDefeat:'24/7',
-			 scoreWin:'12/24',
-		 	 matchHost:'quaidabad'}},
-		{heading:'The PSL',season:'2017',
-			 wiki:'www.wiki.com/psl',
-			 imgSrc:'../images/myimg.jpg',
-			 moreDetails:{
-				 images:[
-				 '../images/my.jpg',
-			 	 'asdasda',
-			 	 'e44',
-				 'jj11',
-				 'hhh22'],
-				 scoreDefeat:'24/7',
-				 scoreWin:'12/24',
-				 matchHost:'landhi'}},
-		{heading:'The ICC',season:'2017',
-		 wiki:'www.wiki.com/psl17',
-		 imgSrc:'../images/myimg17.jpg',
-		 moreDetails:{
-			 images:[
-				 '../images/my.jpg',
-			 	 'asdasda',
-			 	 'adsfda'],
-			 scoreDefeat:'69/7',
-			 scoreWin:'1/240',
-		 	 matchHost:'nadi kinare'}}
-		];
+var dataList = [{name:'The PSL',
+				 seasons:[
+					 
+					 {name:'2015',
+					  wiki:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  scoreDefeat:'24/7',
+						  scoreWin:'12/24',
+						  matchHost:'quaidabad'}},
+					 
+					 {name:'2017',
+					  wiki:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  scoreDefeat:'24/7',
+						  scoreWin:'12/24',
+						  matchHost:'quaidabad'}},
+					 
+					 {name:'2019',
+					  wiki:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  scoreDefeat:'24/7',
+						  scoreWin:'12/24',
+						  matchHost:'quaidabad'}}
+				 ]},
+				
+				{name:'The ICC',
+				 seasons:[
+					 
+					 {name:'2015',
+					  wiki:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  scoreDefeat:'24/7',
+						  scoreWin:'12/24',
+						  matchHost:'quaidabad'}},
+					 
+					 {name:'2017',
+					  wiki:'www.wiki.com/psl', 
+					  imgSrc:'../images/myimg.jpg',
+					  moreDetails:{
+						  images:[
+							  '../images/my.jpg',
+							  'asdasda',
+							  'adsfda'],
+						  scoreDefeat:'24/7',
+						  scoreWin:'12/24',
+						  matchHost:'quaidabad'}}
+				 ]}
+				
+			   ];
+	
+//		{heading:'The PSL',season:'2015',
+//		 wiki:'www.wiki.com/psl',
+//		 imgSrc:'../images/myimg.jpg',
+//		 moreDetails:{
+//			 images:[
+//				 '../images/my.jpg',
+//			 	 'asdasda',
+//			 	 'adsfda'],
+//			 scoreDefeat:'24/7',
+//			 scoreWin:'12/24',
+//		 	 matchHost:'quaidabad'}},
+//	
+//		{heading:'The PSL',season:'2017',
+//			 wiki:'www.wiki.com/psl',
+//			 imgSrc:'../images/myimg.jpg',
+//			 moreDetails:{
+//				 images:[
+//				 '../images/my.jpg',
+//			 	 'asdasda',
+//			 	 'e44',
+//				 'jj11',
+//				 'hhh22'],
+//				 scoreDefeat:'24/7',
+//				 scoreWin:'12/24',
+//				 matchHost:'landhi'}},
+//		{heading:'The ICC',season:'2017',
+//		 wiki:'www.wiki.com/psl17',
+//		 imgSrc:'../images/myimg17.jpg',
+//		 moreDetails:{
+//			 images:[
+//				 '../images/my.jpg',
+//			 	 'asdasda',
+//			 	 'adsfda'],
+//			 scoreDefeat:'69/7',
+//			 scoreWin:'1/240',
+//		 	 matchHost:'nadi kinare'}}
+//		];
---
+++ js/events.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	
+	$('.left-ul span').click(function(e){
+		$(this).toggleClass('fa-caret-right');
+		$(this).toggleClass('fa-caret-down');
+	});
+	
+});

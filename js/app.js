283d54ec0b8f414851ac8b1416aeeb3bca767ac9
awais69 <awais69169@gmail.com>
2019-01-30 12:29:39 +0500

    manage dataList

--- js/app.js
+++ js/app.js
@@ -2,25 +2,49 @@
 
 var events = angular.module("events",[]);
 
-events.controller("categoriesControl",function($scope){
+events.controller("categoriesControl",function($scope,$filter){
 	$scope.showCat = function(event){
 		var clickedRefer = event.currentTarget;
 		var category = clickedRefer.innerHTML;
 		
-		if(category == "The PSL"){
-			$scope.eventsArray = ["PSL1","PSL2","PSL3"];
-		}else{
-			$scope.eventsArray = ["ICC1","Icc2","ICC3"];
-		}
-		
+		$scope.eventsArray = $filter('filter')(dataList, {heading: category});
 	};
 	
-	$scope.eventsArray = [
-		{heading:'PSL',year:'2015',
-		 wiki:'www.wiki.com/psl',
-		 imgSrc:'../images/myimg.jpg'},
-		{heading:'PSL',year:'2017',
-		 wiki:'www.wiki.com/psl17',
-		 imgSrc:'../images/myimg17.jpg'}
-		];
+	$scope.eventsArray = $filter('filter')(dataList, {heading: 'The PSL'});
 });
+
+var dataList = [
+		{heading:'The PSL',season:'2015',
+		 wiki:'www.wiki.com/psl',
+		 imgSrc:'../images/myimg.jpg',
+		 moreDetails:{
+			 images:{
+				 image1:'../images/my.jpg',
+			 	 image2:'asdasda',
+			 	 image3:'adsfda'},
+			 scoreDefeat:'24/7',
+			 scoreWin:'12/24',
+		 	 matchHost:'quaidabad'}},
+		{heading:'The PSL',season:'2017',
+			 wiki:'www.wiki.com/psl',
+			 imgSrc:'../images/myimg.jpg',
+			 moreDetails:{
+				 images:{
+					 image1:'../images/my.jpg',
+					 image2:'asdasda',
+					 image3:'adsfda'},
+				 scoreDefeat:'24/7',
+				 scoreWin:'12/24',
+				 matchHost:'landhi'}},
+		{heading:'The ICC',season:'2017',
+		 wiki:'www.wiki.com/psl17',
+		 imgSrc:'../images/myimg17.jpg',
+		 moreDetails:{
+			 images:{
+				 image1:'../images/myfor3.jpg',
+			 	 image2:'axzxz',
+			 	 image3:'iii'},
+			 scoreDefeat:'69/7',
+			 scoreWin:'1/240',
+		 	 matchHost:'nadi kinare'}}
+		];

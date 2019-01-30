aca06f03a2ea40c7ddd9f530989090d92359dccf
awais69 <awais69169@gmail.com>
2019-01-30 22:11:43 +0500

    tyari chal rhi hai makan ki

--- js/events.js
+++ js/events.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
 	$('.left-ul span').click(function(e){
 		$(this).toggleClass('fa-caret-right');
 		$(this).toggleClass('fa-caret-down');
+		$ul = $(this).parent().parent().children().eq(1);
+		if($ul.hasClass('left-ul-hidden-list')){
+			$ul.addClass('left-ul-show-list');
+			$ul.removeClass('left-ul-hidden-list');
+			$ul.children().css("animation","0.8s fadeInLeft");
+		}else{
+			$ul.children().css("animation","none");
+			$ul.removeClass('left-ul-show-list');
+			$ul.addClass('left-ul-hidden-list');
+		}
 	});
 	
 });

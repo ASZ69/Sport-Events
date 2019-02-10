a7ecd5ab701d3a0d80684625e22ccf44f2106913
awais69 <awais69169@gmail.com>
2019-02-10 21:40:27 +0500

    making themes

--- js/manageTheme.js
+++ js/manageTheme.js
@@ -18,20 +18,22 @@ function changeTheme(ref){
 function loadTheme(selected){
 	switch(selected){
 		case "Standard":
-			document.documentElement.style.setProperty('--hover', '#00FFF3');
-			document.documentElement.style.setProperty('--index-overlay', 'rgba(0,255,243,0.55)');
-			document.documentElement.style.setProperty('--t-md', 'white');
-			document.documentElement.style.setProperty('--t-sm', 'white');
-			document.documentElement.style.setProperty('--bg-cd', '#ccc');
-			document.documentElement.style.setProperty('--cd-t-sm', 'black');
-			document.documentElement.style.setProperty('--cd-t-md', 'black');
-			document.documentElement.style.setProperty('--bg', '#111');
-			document.documentElement.style.setProperty('--bg-rev', '#fff');
+			document.documentElement.style.setProperty('--hover', '#FCFFE5');
+			document.documentElement.style.setProperty('--index-overlay', 'rgba(252,255,229,0.65)');
+			document.documentElement.style.setProperty('--hover-btn', 'rgba(252,255,229,1)');
+			document.documentElement.style.setProperty('--t-md', '#D03E3D');
+			document.documentElement.style.setProperty('--t-sm', '#00c07f');
+			document.documentElement.style.setProperty('--bg-cd', '#262D36');
+			document.documentElement.style.setProperty('--cd-t-sm', '#00c07f');
+			document.documentElement.style.setProperty('--cd-t-md', '#D03E3D');
+			document.documentElement.style.setProperty('--bg', '#313d4b');
+			document.documentElement.style.setProperty('--bg-rev', '#f9f8fd');
 		   break;
 			
 		case "Day":
 			document.documentElement.style.setProperty('--hover', '#00FFF3');
 			document.documentElement.style.setProperty('--index-overlay', 'rgba(0,255,243,0.55)');
+			document.documentElement.style.setProperty('--hover-btn', 'rgba(0,255,243,1)');
 			document.documentElement.style.setProperty('--t-md', 'black');
 			document.documentElement.style.setProperty('--t-sm', 'black');
 			document.documentElement.style.setProperty('--bg-cd', '#fff');
@@ -44,6 +46,7 @@ function loadTheme(selected){
 		case "Night":
 			document.documentElement.style.setProperty('--hover', '#FF6701');
 			document.documentElement.style.setProperty('--index-overlay', 'rgba(255,87,0,0.55)');
+			document.documentElement.style.setProperty('--hover-btn', 'rgba(255,87,0,1)');
 			document.documentElement.style.setProperty('--t-md', 'white');
 			document.documentElement.style.setProperty('--t-sm', 'white');
 			document.documentElement.style.setProperty('--bg-cd', '#ccc');
